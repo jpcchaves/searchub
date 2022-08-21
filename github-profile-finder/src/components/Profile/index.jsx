a53da433bc26c9efded56a7e520fc2846223ac70
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-08-21 20:24:36 -0300

    added icons and profile e-mail, bio and url

--- github-profile-finder/src/components/Profile/index.jsx
+++ github-profile-finder/src/components/Profile/index.jsx
@@ -1,7 +1,10 @@
 // CSS
 import styles from "./Profile.module.css";
-
+// Hooks
 import { useEffect, useState } from "react";
+
+// Icons
+import { BsFillGeoAltFill, BsEnvelopeFill, BsGithub } from "react-icons/bs";
 
 const Profile = ({ userName }) => {
   // GitHub Data
@@ -15,8 +18,9 @@ const Profile = ({ userName }) => {
     const getProfile = async (url) => {
       const res = await fetch(url);
       const profileDataFetch = await res.json();
-
-      setProfileData(profileDataFetch);
+      if (profileDataFetch) {
+        setProfileData(profileDataFetch);
+      }
     };
     getProfile(githubURL);
   }, [githubURL]);
@@ -29,10 +33,34 @@ const Profile = ({ userName }) => {
             <img src={profileData.avatar_url} alt={profileData.name} />
             <p className={styles.profile_name}>{profileData.name}</p>
             <p className={styles.profile_login}>{profileData.login}</p>
-            <p className={styles.profile_bio}>{profileData.bio}</p>
+            <p className={styles.profile_bio}>
+              {profileData.bio || "Não informado"}
+            </p>
+            <p className={styles.profile_location}>
+              <BsFillGeoAltFill />
+              {profileData.location || "Não informado"}
+            </p>
+            <p className={styles.profile_email}>
+              <BsEnvelopeFill />
+              {profileData.email || "Não informado"}
+            </p>
+            <p className={styles.profile_link}>
+              <BsGithub />
+              <a
+                href={profileData.html_url}
+                target="_blank"
+                className={styles.profile_url}
+              >
+                Acessar perfil
+              </a>
+            </p>
           </>
         ) : (
-          <>{userName.length > 0 && <p>Usuário não encontrado!</p>}</>
+          <>
+            {profileData && userName.length > 0 && (
+              <p>Usuário não encontrado!</p>
+            )}
+          </>
         )}
       </div>
     </>
