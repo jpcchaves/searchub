841b208d659df8ea83e4a7b22f2ea69f798284fa
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-08-22 18:29:58 -0300

    == reestructuring the project files ==
    There was ocoriyng some errors, so i decided to reorganize the project files

--- github-profile-finder/src/components/Profile/index.jsx
+++ github-profile-finder/src/components/Profile/index.jsx
@@ -6,66 +6,69 @@ import { useEffect, useState } from "react";
 // Icons
 import { BsFillGeoAltFill, BsEnvelopeFill, BsGithub } from "react-icons/bs";
 
-const Profile = ({ userName }) => {
+const Profile = () => {
   // GitHub Data
   const [profileData, setProfileData] = useState("");
 
   // Loading State
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   // GitHub URL
-  const githubURL = `https://api.github.com/users/${userName}`;
+  const githubURL = `https://api.github.com/users/`;
 
   // GitHub Profile fetch
   useEffect(() => {
     const getProfile = async () => {
+      setLoading(true);
+      setError(false);
       try {
         const res = await fetch(githubURL);
         const profileDataFetch = await res.json();
         setProfileData(profileDataFetch);
-        setLoading(true);
       } catch (error) {
-        setLoading(true);
+        setError(true);
       }
+      setLoading(false);
     };
     getProfile();
-  }, [githubURL]);
+  }, []);
 
   return (
     <>
+      <form id={styles.form_wrapper}>
+        <h3>Procure seu Perfil no GitHub</h3>
+        <label className={styles.label}>
+          <span>Digite seu nome de usuário:</span>
+          <input type="text" placeholder="Nome de usuário..." />
+          <input type="submit" value="Buscar" />
+        </label>
+      </form>
       <div id={styles.github_profile}>
-        {profileData && !loading ? (
-          <>
-            <img src={profileData.avatar_url} alt={profileData.name} />
-            <p className={styles.profile_name}>{profileData.name}</p>
-            <p className={styles.profile_login}>{profileData.login}</p>
-            <p className={styles.profile_bio}>
-              {profileData.bio || "Não informado"}
-            </p>
-            <p className={styles.profile_location}>
-              <BsFillGeoAltFill />
-              {profileData.location || "Não informado"}
-            </p>
-            <p className={styles.profile_email}>
-              <BsEnvelopeFill />
-              {profileData.email || "Não informado"}
-            </p>
-            <p className={styles.profile_link}>
-              <BsGithub />
-              <a
-                href={profileData.html_url}
-                target="_blank"
-                className={styles.profile_url}
-              >
-                Acessar perfil
-              </a>
-            </p>
-          </>
-        ) : (
-          <>
-            {userName.length > 0 && loading && <p>Usuário não encontrado!</p>}
-          </>
-        )}
+        <img src={profileData.avatar_url} alt={profileData.name} />
+        <p className={styles.profile_name}>{profileData.name}</p>
+        <p className={styles.profile_login}>{profileData.login}</p>
+        <p className={styles.profile_bio}>
+          {profileData.bio || "Não informado"}
+        </p>
+        <p className={styles.profile_location}>
+          <BsFillGeoAltFill />
+          {profileData.location || "Não informado"}
+        </p>
+        <p className={styles.profile_email}>
+          <BsEnvelopeFill />
+          {profileData.email || "Não informado"}
+        </p>
+        <p className={styles.profile_link}>
+          <BsGithub />
+          <a
+            href={profileData.html_url}
+            target="_blank"
+            className={styles.profile_url}
+          >
+            Acessar perfil
+          </a>
+        </p>
       </div>
     </>
   );
--- github-profile-finder/src/components/SearchForm/index.jsx
+++ github-profile-finder/src/components/SearchForm/index.jsx
@@ -1,38 +1,12 @@
 // CSS
-import { useState } from "react";
 import Advices from "../Advices";
 import Profile from "../Profile";
 import styles from "./SearchForm.module.css";
 
 const SearchForm = () => {
-  const [profileName, setProfileName] = useState("");
-  const [finalName, setFinalName] = useState("");
-
-  console.log(profileName);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
   return (
-    <div id={styles.form_wrapper}>
-      <h3>Procure seu Perfil no GitHub</h3>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <label className={styles.label}>
-          <span>Digite seu nome de usuário:</span>
-          <input
-            type="text"
-            placeholder="Nome de usuário..."
-            onChange={(e) => setProfileName(e.target.value)}
-          />
-          <input
-            type="submit"
-            value="Buscar"
-            onClick={() => setFinalName(profileName)}
-          />
-        </label>
-      </form>
-      <Profile userName={finalName} />
+    <div>
+      <Profile />
       <Advices />
     </div>
   );
