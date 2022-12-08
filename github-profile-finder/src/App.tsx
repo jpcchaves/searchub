8cd93454618b9bc8e1598bc1a644f1793fd08b05
Joao Paulo <joaopaulodecastrochaves@hotmail.com>
2022-12-07 22:43:54 -0300

    refactor: add typescript, eslint and prettier config. remove components to reestructure the files

---
+++ github-profile-finder/src/App.tsx
@@ -0,0 +1,11 @@
+import { GlobalStyle } from '../styles/GlobalStyles';
+import SearchForm from './pages/SearchForm';
+
+export const App = () => {
+	return (
+		<>
+			<SearchForm />
+			<GlobalStyle />
+		</>
+	);
+};
